d031d9fddff4b610cf2f200fccf3db31630ba8a0
James <activedge@Jamess-MacBook-Pro.local>
2019-12-09 18:54:22 +0100

    broken homepage into components and styled components

---
+++ src/components/menu-items/menu-items.components.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './menu-items.styles.scss';
+
+const menuItem = ({title,imageUrl, size}) => {
+    return (
+        <div className={`${size} menu-item`} style={{backgroundImage: `url(${imageUrl})`}}>
+            <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}></div>
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                    <span className="subtitle">SHOP NOW</span>
+        </div>
+        </div>
+    )
+}
+export default menuItem;
